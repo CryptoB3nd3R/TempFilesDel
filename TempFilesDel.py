47811248c9da4c75f311e306e7bfe7a5467546fe
CryptoB3nd3R <119752156+CryptoB3nd3R@users.noreply.github.com>
2024-06-20 23:15:45 -0400

    Add files via upload

---
+++ TempFilesDel.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+#author: cryptoB3nD3R
+
+import os
+import shutil
+
+    
+folder = 'C:/Users/'+os.getlogin()+'/AppData/Local/Temp'
+
+deleteFileCount = 0
+deleteFolderCount = 0
+
+for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    indexNo = file_path.find('\\')
+    itemName = file_path[indexNo+1:]
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+            print( '%s file deleted' % itemName )
+            deleteFileCount = deleteFileCount + 1
+
+        elif os.path.isdir(file_path):
+            if file_path.__contains__('chocolatey'):  continue
+            shutil.rmtree(file_path)
+            print( '%s folder deleted' % itemName )
+            deleteFolderCount = deleteFolderCount + 1
+
+    except Exception as e:
+        print('Access Denied: %s' % itemName )
+
+print(str(deleteFileCount)+' files and '+ str(deleteFolderCount)+' folders deleted.')
+input('Press <Enter> to Exit')
